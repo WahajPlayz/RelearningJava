6b84e1bbbb611d3996f80f97086da48229a2f627
WahajPlayz <161223768+WahajPlayz@users.noreply.github.com>
2026-04-24 20:45:02 +0100

    The START

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am Relearning Java Again in the big 2026!!");
+
+        // I am using this playlist it shows how to learn java for Minecraft Modding and Hytale if you want to learn Hytale but this is the latest tutorial for learning Java for minecraft modding
+        // https://www.youtube.com/watch?v=Tl0PTjNayx4&list=PLKGarocXCE1EBIOx2UTpGPZyWMpanSnDj
+    }
+}
