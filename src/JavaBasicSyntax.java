ee4d315633f5f8c272d1e1d1220913ba418501b8
WahajPlayz <161223768+WahajPlayz@users.noreply.github.com>
2026-04-25 18:06:36 +0100

    Learning Java Syntax

---
+++ src/JavaBasicSyntax.java
@@ -0,0 +1,21 @@
+public class JavaBasicSyntax {
+    public static void main(String[] args) {
+        /* JAVA SYNTAX BASICS */
+
+        /* Variables */
+
+        // datatype name_of_variable; //Declaration
+        // OR
+        // datatype name_of_variable = value_of_variable; // Initialization ( Declaration + Assignment of Value )
+
+        int points;     // Declaration
+        points = 100;   // Assignment
+        int x = -69;    // Initialization ( Declaration + Assignment of Value )
+
+        // Every Instruction/Line ends with a semicolon ;
+        // int y = 42
+
+
+
+    }
+}
